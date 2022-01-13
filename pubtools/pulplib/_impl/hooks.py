00a293d22dc993ce583963f17cd4dcff3dc0ee23
Nathan Gillett <ngillett@redhat.com>
2022-01-13 18:02:01 -0500

    Add pulp_repository_pre_publish hookspec [RHELDST-7309]
    Activated as the first step in publishing a repository.
    This may be used to subscribe to all repository publish events
    triggered by this library and allows hook implementers the opportunity
    to adjust publish options.

--- pubtools/pulplib/_impl/hooks.py
+++ pubtools/pulplib/_impl/hooks.py
@@ -7,13 +7,36 @@ from pubtools.pluggy import hookspec, pm
 
 
 @hookspec
+def pulp_repository_pre_publish(repository, options):
+    """Invoked as the first step in publishing a Pulp repository.
+
+    If a hookimpl returns a non-None value, that value will be used to replace
+    the options for this publish. This can be used to adjust publish options
+    from within a hook.
+
+    Args:
+        repository (:class:`~pubtools.pulplib.Repository`):
+            The repository to be published.
+
+        options (:class:`~pubtools.pulplib.PublishOptions`):
+            The options to use in publishing.
+
+    Returns:
+        options (:class:`~pubtools.pulplib.PublishOptions`):
+            The potentially adjusted options used for this publish.
+    """
+
+
+@hookspec
 def pulp_repository_published(repository, options):
     """Invoked after a Pulp repository has been successfully published.
 
-    :param repository: the repository which has been published.
-    :type repository: pubtools.pulplib.Repository
-    :param options: the options used for this publish.
-    :type options: pubtools.pulplib.PublishOptions
+    Args:
+        repository (:class:`~pubtools.pulplib.Repository`):
+            The repository which has been published.
+
+        options (:class:`~pubtools.pulplib.PublishOptions`):
+            The options used for this publish.
     """
 
 
