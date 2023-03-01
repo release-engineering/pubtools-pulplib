6815f454fd5eb35d002a23e0136b664958d17397
Jindrich Luza <jluza@redhat.com>
2023-03-01 10:34:05 +0100

    Fixed doc strings

--- tests/model/test_errata_model.py
+++ tests/model/test_errata_model.py
@@ -9,7 +9,7 @@ from pubtools.pulplib import InvalidDataException
 
 
 def test_container_list(data_path):
-    """Provide invalid structure object to container_list attribute."""
+    """Test load of valid errata structure."""
 
     with open(os.path.join(data_path, "sample-erratum.json"), "rt") as f:
         erratum_data = json.loads(f.read())
@@ -18,7 +18,7 @@ def test_container_list(data_path):
 
 
 def test_container_list_invalid(data_path):
-    """Test load of valid errata structure."""
+    """Provide invalid structure object to container_list attribute."""
 
     with open(
         os.path.join(data_path, "sample-erratum-invalid-container-list1.json"), "rt"
