56e0603b9693c9df9a15d3ab1942d4a78fe88ea6
Michal Haluza <mhaluza@redhat.com>
2023-04-03 13:45:34 +0200

    Revert "Remove workarounds for old version of attrs library"
    This reverts commit 73e2568da443dea47c79149e64762311a1acbf01.
    
    The workarounds are still needed on RHEL 8 where the python 3.6
    libraries are installed from RPMs as RHEL 8 ships attrs 17.4

--- pubtools/pulplib/_impl/compat_attr.py
+++ pubtools/pulplib/_impl/compat_attr.py
@@ -4,17 +4,17 @@ ATTR_VERSION = tuple(int(x) for x in attr.__version__.split(".")[0:2])
 
 # pylint: disable=invalid-name
 
-# Set some defaults when using attr
+# Wrappers around attr module to handle some incompatibilities with older versions of the module.
 
 
 def s(*args, **kwargs):
-    # A few features we'd like to use are not available on older Python/attrs
+    # A few features we'd like to use are not available on older attrs
     # versions, so we just silently disable them.
     #
     # Note this means we're technically exporting a different API on
-    # Py2 vs Py3, i.e. keywords-only isn't enforced on Py2.
+    # older versions of attr, i.e. keywords-only isn't enforced.
     # This isn't ideal, but is mitigated by the fact that any new code
-    # using this library will surely not be tested solely on Py2.
+    # using this library will surely not be tested solely using old versions of attr.
     kwargs = kwargs.copy()
 
     if "slots" not in kwargs:
@@ -28,10 +28,27 @@ def s(*args, **kwargs):
         # don't use the attrs-generated repr by default
         kwargs["repr"] = False
 
+    if "kw_only" in kwargs and ATTR_VERSION < (18, 2):  # pragma: no cover
+        # This is only implemented in attrs 18.2 and newer.
+        # attr.s() will raise if kw_only is provided in older version of attr.
+        del kwargs["kw_only"]
+
     return attr.s(*args, **kwargs)
 
 
-fields_dict = attr.fields_dict
+if ATTR_VERSION < (18, 1):  # pragma: no cover
+    # older attrs version didn't provide this function yet,
+    # but it's easily added here.
+    def fields_dict(cls):
+        out = {}
+        for field in attr.fields(cls):
+            out[field.name] = field
+        return out
+
+else:
+    fields_dict = attr.fields_dict
+
+
 ib = attr.ib
 Factory = attr.Factory
 fields = attr.fields
